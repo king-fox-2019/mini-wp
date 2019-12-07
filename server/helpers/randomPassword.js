9fa74a6cbcda12e438009d9c06a3e8366794bd26
sukmabrahmantya <brahmantyasukma@gmail.com>
2019-12-07 14:43:11 +0700

    adding bcrypt and random password helpers in server side

---
+++ server/helpers/randomPassword.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = () => {
+  const length = 8
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  let password = "";
+
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    password += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return password;
+}
