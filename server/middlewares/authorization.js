fb1e09436484d29984d8f323e5eef7bb412e97b3
sukmabrahmantya <brahmantyasukma@gmail.com>
2019-12-07 14:41:09 +0700

    adding authorization and authentication middlewares in server side

---
+++ server/middlewares/authorization.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const { Article } = require('../models')
+
+module.exports = (req, res, next) => {
+  Article
+    .findById({ _id: req.params.id })
+    .then(article => {
+      if (article.userId == req.decoded._id) {
+        next()
+      } else {
+        next({ status: 403, message: `You dont have authorize to do that` })
+      }
+    })
+    .catch(err => { next(err) })
+}
+
+
+
+
