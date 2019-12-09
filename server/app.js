2e7a20dea6ef2cb0538ffabf8f75949179059f6f
root <root@ip-172-31-20-19.us-east-2.compute.internal>
2019-12-09 09:03:40 +0000

    commit dari server, app.js yang di lokal bukan buat production.

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ if(process.env.NODE_ENV == 'development') require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 55555
 const mongoose = require('mongoose')
 const cors = require('cors')
 const morgan = require('morgan')
@@ -13,11 +13,14 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-mongoose.connect(`mongodb://localhost:27017/miniwpweekend`, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true
+mongoose.connect(`mongodb+srv://aws-miniwp-zoe:Ravepro203@hikari0-rmg8u.mongodb.net/test?retryWrites=true&w=majority`, {
+   useNewUrlParser: false
 })
 
 app.listen(port, () => console.log(`listening on port ${port}`))
+
+app.get('/hellow', (req, res) => {
+	res.json({message:'hello'})
+})
 
 app.use(router)
