519fea87955e9874e17de223ffd3647531291283
deprak <dedprakasa@gmail.com>
2019-12-09 07:15:56 +0700

    refactor errorHandler

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,18 +1,18 @@
 module.exports = (err, req, res, next) => {
   let status, message;
   if (err.name) {
-      switch (err.name) {
-          case "CastError":
-              status = err.status || 500;
-              message = err.message || "Internal server error"
-          break;
-          default:
-              status = 500;
-              message = `${err.name} - Internal server error`
-      }
+    switch (err.name) {
+      case "CastError":
+        status = err.status || 500;
+        message = err.message || "Internal server error"
+      break;
+      default:
+        status = 500;
+        message = `${err.name} - Internal server error`
+    }
   } else {
-      status = err.status;
-      message = err.message;
+    status = err.status;
+    message = err.message;
   }
   res.status(status).json({ message: message });
 }
