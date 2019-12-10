e92847389d385a4e0e4acd83c155f846d9e68268
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-12-10 13:07:01 +0700

    change mongo connect setting

--- client/src/helpers/axios.js
+++ client/src/helpers/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:3003'
-  // baseURL: 'http://34.87.67.96'
+  // baseURL: 'http://localhost:3003'
+  baseURL: 'http://34.87.67.96'
 })
