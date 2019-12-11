3db145a1ac65e766fa9c97c9f3ceddf10fc8662c
michaelwp <michael.wenceslaus@gmail.com>
2019-12-11 10:38:47 +0700

    add google id prod

--- client/src/components/loginPage/loginFormComponent.vue
+++ client/src/components/loginPage/loginFormComponent.vue
@@ -55,8 +55,8 @@
                 email: null,
                 password: null,
                 googleSignInParams: {
-                    // client_id: '1077663190419-heca6gdre9f5031fdtd57m2uts9u64jj.apps.googleusercontent.com' //prodServer
-                    client_id: '1077663190419-fkbutpat7sloejjac79gs8tnfa1bmi3k.apps.googleusercontent.com' //localhost:1234'
+                    client_id: '1077663190419-heca6gdre9f5031fdtd57m2uts9u64jj.apps.googleusercontent.com' //prodServer
+                    // client_id: '1077663190419-fkbutpat7sloejjac79gs8tnfa1bmi3k.apps.googleusercontent.com' //localhost:1234'
                 },
                 header: null,
                 content: null,
--- client/src/main.js
+++ client/src/main.js
@@ -6,8 +6,8 @@ import VueQuill from 'vue-quill';
 import GAuth from 'vue-google-oauth2';
 
 const gauthOption = {
-    // clientId: '1077663190419-heca6gdre9f5031fdtd57m2uts9u64jj.apps.googleusercontent.com' //prodServer
-    clientId: '1077663190419-fkbutpat7sloejjac79gs8tnfa1bmi3k.apps.googleusercontent.com' //localhost:1234
+    clientId: '1077663190419-heca6gdre9f5031fdtd57m2uts9u64jj.apps.googleusercontent.com' //prodServer
+    // clientId: '1077663190419-fkbutpat7sloejjac79gs8tnfa1bmi3k.apps.googleusercontent.com' //localhost:1234
 };
 
 Vue.use(GAuth, gauthOption);
